aeb29b18a68da0a980e4bb7b5a689da94ade76c0
Al Faridul Karim <alfaridulkarim300804@gmail.com>
2026-05-14 04:32:25 +0600

    chore: remove temporary inspection scripts

--- inspect_prepare.py
+++
@@ -1,8 +0,0 @@
-import json
-path = "notebooks/neuromark_real_tribe_brainplot_api_colab.ipynb"
-with open(path, "r", encoding="utf-8") as f: nb = json.load(f)
-for c in nb["cells"]:
-    if c["cell_type"] == "code":
-        src = "".join(c["source"])
-        if "def prepare_asset_for_tribe" in src:
-            print(src)
